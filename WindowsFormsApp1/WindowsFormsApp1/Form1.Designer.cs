869c8bb1a90db646ee8624f38c654749ca23342b
Josh <jcttierra@gmail.com>
2018-03-07 20:02:31 -0500

    Initial Commit
    WPF app: drag/drop images to send to Computer Vision API to receive output in textarea. Currently text isn't displaying

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class PictureLoader
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(PictureLoader));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.loadToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pbImage = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbImage)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem1});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(384, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.loadToolStripMenuItem});
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(37, 20);
+            this.toolStripMenuItem1.Text = "File";
+            // 
+            // loadToolStripMenuItem
+            // 
+            this.loadToolStripMenuItem.Name = "loadToolStripMenuItem";
+            this.loadToolStripMenuItem.Size = new System.Drawing.Size(100, 22);
+            this.loadToolStripMenuItem.Text = "Load";
+            this.loadToolStripMenuItem.Click += new System.EventHandler(this.loadToolStripMenuItem_Click);
+            // 
+            // pbImage
+            // 
+            this.pbImage.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.pbImage.Image = ((System.Drawing.Image)(resources.GetObject("pbImage.Image")));
+            this.pbImage.Location = new System.Drawing.Point(0, 27);
+            this.pbImage.Name = "pbImage";
+            this.pbImage.Size = new System.Drawing.Size(384, 235);
+            this.pbImage.TabIndex = 1;
+            this.pbImage.TabStop = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(0, 269);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(384, 207);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "Nothing to show yet!";
+            // 
+            // PictureLoader
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(384, 475);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pbImage);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.Name = "PictureLoader";
+            this.Text = "Computer Vision";
+            this.Load += new System.EventHandler(this.PictureLoader_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbImage)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem loadToolStripMenuItem;
+        private System.Windows.Forms.PictureBox pbImage;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.ProjectOxford.Vision;
+using Microsoft.ProjectOxford.Vision.Contract;
+
+namespace WindowsFormsApp1
+{
+    public partial class PictureLoader : Form
+    {
+        //Set Azure account key
+        const string SubscriptionKey = "7b2c54a81f31425e92bb9c807b3a2953";
+        public PictureLoader()
+        {
+            InitializeComponent();
+            //Clear PictureBox
+            pbImage.Image.Dispose();
+            pbImage.Image = null;
+            //Load new image from file
+            pbImage.Image = Image.FromFile(System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal) + @"\puppy.jpg");
+            //PictureBox size setting = adjust size of image
+            pbImage.SizeMode = PictureBoxSizeMode.AutoSize;
+            //Form size setting = adjust to pbImage size
+            this.ClientSize = new Size(pbImage.Image.Width, pbImage.Image.Height + toolStripMenuItem1.Height + textBox1.Height);
+            //Position Image to top left corner of form
+            pbImage.Location = new System.Drawing.Point(0, toolStripMenuItem1.Height);
+            //Position TextBox right below ImageBox
+            textBox1.Location = new System.Drawing.Point(0, pbImage.Image.Height + toolStripMenuItem1.Height);
+            //Readjust width of TextBox
+            textBox1.Width = pbImage.Width;
+        }
+
+        private void PictureLoader_Load(object sender, EventArgs e)
+        {
+            this.AllowDrop = true;
+            this.DragEnter += Form1_DragEnter;
+            this.DragDrop += Form1_DragDrop;
+        }
+        
+        private void Form1_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                e.Effect = DragDropEffects.Copy;
+            }
+            else
+            {
+                e.Effect = DragDropEffects.None;
+            }
+        }
+        private void Form1_DragDrop(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                //String array of file paths dropped
+                string[] filePaths = (string[])(e.Data.GetData(DataFormats.FileDrop));
+                //Pass first file path to isImage
+                isImage(filePaths[0]);
+            }
+        }
+
+        private void loadToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Multiselect = false;
+            if (openFileDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                string fileName = openFileDialog.FileName;
+                isImage(fileName);
+            }
+        }
+        //Loads file if it is an image
+        //Args: file path in a string
+        private void isImage(string fileName)   
+        {
+            textBox1.Text = "";
+            //Check if file type is an image
+            if (fileName.Contains(".bmp") || fileName.Contains(".tif") || fileName.Contains(".jpg") || fileName.Contains(".gif") || fileName.Contains(".png"))
+            {
+                //Clear PictureBox prior to loading new image
+                if(pbImage.Image != null)
+                {
+                    pbImage.Image.Dispose();
+                    pbImage.Image = null;
+                }
+                //Load new file from path String fileName
+                pbImage.Image = Image.FromFile(fileName);
+                //Resize form size to picture size
+                this.ClientSize = new Size(pbImage.Image.Width, pbImage.Image.Height + toolStripMenuItem1.Height + textBox1.Height);
+                textBox1.Location = new System.Drawing.Point(0, pbImage.Image.Height + toolStripMenuItem1.Height);
+                textBox1.Width = pbImage.Width;
+
+                //Test
+                Uri uri = new Uri(fileName, UriKind.Absolute);
+                var done = DoWork(uri, true);
+            } else {
+                MessageBox.Show("File not an image of type:\n.bmp\n.tif\n.jpg\n.gif\n.png", "File Error", MessageBoxButtons.OK, MessageBoxIcon.None,
+                    MessageBoxDefaultButton.Button1, (MessageBoxOptions)0x40000);
+            }
+        }
+        /// <summary>
+        /// Uploads the image to Project Oxford and performs analysis
+        /// </summary>
+        /// <param name="imageFilePath">The image file path.</param>
+        /// <returns></returns>
+        private async Task<AnalysisResult> UploadAndAnalyzeImage(string imageFilePath)
+        {
+            // -----------------------------------------------------------------------
+            // KEY SAMPLE CODE STARTS HERE
+            // -----------------------------------------------------------------------
+
+            //
+            // Create Project Oxford Vision API Service client
+            //
+            VisionServiceClient VisionServiceClient = new VisionServiceClient(SubscriptionKey, "https://westcentralus.api.cognitive.microsoft.com/vision/v1.0");
+
+            using (Stream imageFileStream = File.OpenRead(imageFilePath))
+            {
+                //
+                // Analyze the image for all visual features
+                //
+                VisualFeature[] visualFeatures = new VisualFeature[] { VisualFeature.Adult, VisualFeature.Categories, VisualFeature.Color, VisualFeature.Description, VisualFeature.Faces, VisualFeature.ImageType, VisualFeature.Tags };
+                AnalysisResult analysisResult = await VisionServiceClient.AnalyzeImageAsync(imageFileStream, visualFeatures);
+                return analysisResult;
+            }
+
+            // -----------------------------------------------------------------------
+            // KEY SAMPLE CODE ENDS HERE
+            // -----------------------------------------------------------------------
+        }
+
+        /// <summary>
+        /// Sends a url to Project Oxford and performs analysis
+        /// </summary>
+        /// <param name="imageUrl">The url of the image to analyze</param>
+        /// <returns></returns>
+        private async Task<AnalysisResult> AnalyzeUrl(string imageUrl)
+        {
+            // -----------------------------------------------------------------------
+            // KEY SAMPLE CODE STARTS HERE
+            // -----------------------------------------------------------------------
+
+            //
+            // Create Project Oxford Vision API Service client
+            //
+            VisionServiceClient VisionServiceClient = new VisionServiceClient(SubscriptionKey);
+
+            //
+            // Analyze the url for all visual features
+            //
+            VisualFeature[] visualFeatures = new VisualFeature[] { VisualFeature.Adult, VisualFeature.Categories, VisualFeature.Color, VisualFeature.Description, VisualFeature.Faces, VisualFeature.ImageType, VisualFeature.Tags };
+            AnalysisResult analysisResult = await VisionServiceClient.AnalyzeImageAsync(imageUrl, visualFeatures);
+            return analysisResult;
+
+            // -----------------------------------------------------------------------
+            // KEY SAMPLE CODE ENDS HERE
+            // -----------------------------------------------------------------------
+        }
+        /// <summary>
+        /// Perform the work for this scenario
+        /// </summary>
+        /// <param name="imageUri">The URI of the image to run against the scenario</param>
+        /// <param name="upload">Upload the image to Project Oxford if [true]; submit the Uri as a remote url if [false];</param>
+        /// <returns></returns>
+        protected async Task<int> DoWork(Uri imageUri, bool upload)
+        {
+            //
+            // Either upload an image, or supply a url
+            //
+            AnalysisResult analysisResult;
+            if (upload)
+            {
+                analysisResult = await UploadAndAnalyzeImage(imageUri.LocalPath);
+            }
+            else
+            {
+                analysisResult = await AnalyzeUrl(imageUri.AbsoluteUri);
+            }
+
+            //Show result of Computer Vision API on TextBow
+            textBox1.Text = "";
+            textBox1.Text = analysisResult.Color.DominantColors.ToString();
+            //textBox1.Text = imageUri.ToString();
+            //textBox1.Text = analysisResult.Description.ToString();
+
+            return 1;
+        }
+    }
+}
